2e2d0ac56db332e1dfdac145fedf8ff64894793c
Mutahira Hafeez <mutahira.2021@gmail.com>
2021-06-01 02:15:48 +0500

    Done with Notification code

--- Notifiactions/app/src/main/java/com/example1/ntificationap/MainActivity.java
+++ Notifiactions/app/src/main/java/com/example1/ntificationap/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         notifyBtn = findViewById(R.id.Notifybtn);
-
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel channel = new NotificationChannel("My Notification","My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
         notifyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
