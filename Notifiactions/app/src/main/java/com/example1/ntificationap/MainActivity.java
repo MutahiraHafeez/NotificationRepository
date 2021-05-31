9590547089278e833d11aca2f84a4e2c12b4475a
Mutahira Hafeez <mutahira.2021@gmail.com>
2021-06-01 02:04:35 +0500

    Notification Practice

--- Notifiactions/app/src/main/java/com/example1/ntificationap/MainActivity.java
+++ Notifiactions/app/src/main/java/com/example1/ntificationap/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example1.ntificationap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
-
+   Button notifyBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notifyBtn = findViewById(R.id.Notifybtn);
+
+        notifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Notifiaction code goes here
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"My Notification");
+                builder.setContentTitle("New Notification");
+                builder.setContentText("Hello from Mutahira, Doing practice with Notification in Android Studio");
+            }
+        });
     }
+
+
+
+
 }
